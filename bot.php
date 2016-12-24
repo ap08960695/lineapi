ef2c29ceb9c0ee2be5acaed8b828099b6f5e49dc
ap08960695 <big_xxkool@hotmail.com>
2016-12-25 02:09:07 +0700

    dd

--- bot.php
+++ bot.php
@@ -1,7 +1,7 @@
 <?php
 $access_token = '8Xgy2cPFKsNPIIFdP9KpS6gVyz5XIcUbreQM2BhEjE5qz3dg6EmXOSScxyMAsAOCC2KNGe3tptY7vzosT0oONJ/CCqEYBnrmo7ljUMbqzRRlQzB8oZGQYsQLlLaXDFgcv0Z3/9Yuoy+gxyYwXbAmTgdB04t89/1O/w1cDnyilFU=';
 
-$url = 'https://api.line.me/v2/bot/profile/ua710308043baa26613bc6e0230cb45fd';
+$url = 'https://api.line.me/v2/bot/profile/u206d25c2ea6bd87c17655609a1c37cb8';
 
 $headers = array('Authorization: Bearer ' . $access_token);
 
