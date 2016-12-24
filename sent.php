16e1f3b5ccdec9cf918d8bbd2ce57ffe82f0cb47
ap08960695 <big_xxkool@hotmail.com>
2016-12-25 03:06:09 +0700

    ddd

--- sent.php
+++ sent.php
@@ -1,9 +1,9 @@
 <?php
 $access_token = '8Xgy2cPFKsNPIIFdP9KpS6gVyz5XIcUbreQM2BhEjE5qz3dg6EmXOSScxyMAsAOCC2KNGe3tptY7vzosT0oONJ/CCqEYBnrmo7ljUMbqzRRlQzB8oZGQYsQLlLaXDFgcv0Z3/9Yuoy+gxyYwXbAmTgdB04t89/1O/w1cDnyilFU=';
 
-$url = 'https://api.line.me/v2/bot/message/push';
+            $url = 'https://api.line.me/v2/bot/message/push';
             $UID = 'U6ad599cf40005784875d3eaee6f6c9b0';
-            $text = "สวัดดีเราเป็นบอท"ว
+            $text = "สวัดดีเราเป็นบอท";
 			// Build message to reply back
 			$messages = [
 				'type' => 'text',
@@ -14,9 +14,9 @@ $url = 'https://api.line.me/v2/bot/message/push';
 				'messages' => [$messages],
 			];
             $post = json_encode($data);
-$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
+            $headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
 
-$ch = curl_init($url);
+            $ch = curl_init($url);
 			curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
 			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 			curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
@@ -25,5 +25,5 @@ $ch = curl_init($url);
 			$result = curl_exec($ch);
 			curl_close($ch);
 
-echo $result;
+            echo $result;
 ?>
