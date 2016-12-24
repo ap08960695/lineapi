fbf5fb55839f6321a66cc900390bc6145676cc50
ap08960695 <big_xxkool@hotmail.com>
2016-12-25 02:39:22 +0700

    d

--- index.php
+++ index.php
@@ -1,3 +1,47 @@
 <?
-echo file_get_contents('php://input');
+$access_token = '8Xgy2cPFKsNPIIFdP9KpS6gVyz5XIcUbreQM2BhEjE5qz3dg6EmXOSScxyMAsAOCC2KNGe3tptY7vzosT0oONJ/CCqEYBnrmo7ljUMbqzRRlQzB8oZGQYsQLlLaXDFgcv0Z3/9Yuoy+gxyYwXbAmTgdB04t89/1O/w1cDnyilFU=';
+
+// Get POST body content
+$content = file_get_contents('php://input');
+// Parse JSON
+$events = json_decode($content, true);
+// Validate parsed JSON data
+if (!is_null($events['events'])) {
+	// Loop through each event
+	foreach ($events['events'] as $event) {
+		// Reply only when message sent is in 'text' format
+		if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
+			// Get text sent
+			$text = $event['message']['text']." ".$event['source']['userId'];
+			// Get replyToken
+			$replyToken = $event['replyToken'];
+
+			// Build message to reply back
+			$messages = [
+				'type' => 'text',
+				'text' => $text
+			];
+
+			// Make a POST Request to Messaging API to reply to sender
+			$url = 'https://api.line.me/v2/bot/message/reply';
+			$data = [
+				'replyToken' => $replyToken,
+				'messages' => [$messages],
+			];
+			$post = json_encode($data);
+			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
+
+			$ch = curl_init($url);
+			curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+			curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+			curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+			curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+			$result = curl_exec($ch);
+			curl_close($ch);
+
+			echo $result . "\r\n";
+		}
+	}
+}
 ?>
